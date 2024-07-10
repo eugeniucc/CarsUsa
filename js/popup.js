b9c61749f3b7a6f244932208fdabba190e82f80e
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-07-10 16:51:16 +0300

    modal overlay added

--- js/popup.js
+++ js/popup.js
@@ -3,24 +3,22 @@ const serviceBtn = document.querySelector(".services_btn");
 const btnClose = document.querySelector(".popup_btn-close");
 const popup = document.querySelector(".popup");
 const popupBtn = document.querySelector(".popup_btn");
+const popupOverlay = document.querySelector(".popup_overlay");
 
 function showModal() {
   popup.classList.add("showPopup");
+  popupOverlay.classList.add("showPopup");
 }
 
 function hideModal() {
   popup.classList.remove("showPopup");
+  popupOverlay.classList.remove("showPopup");
 }
 
 function hideOnKey(e) {
   if (e.key === "Escape") {
     popup.classList.remove("showPopup");
-  }
-}
-
-function hideOnClick(e) {
-  if (!popup.contains(e.target)) {
-    popup.classList.remove("showPopup");
+    popupOverlay.classList.remove("showPopup");
   }
 }
 
@@ -28,6 +26,7 @@ document.addEventListener("keydown", hideOnKey);
 
 headerBtn.addEventListener("click", showModal);
 serviceBtn.addEventListener("click", showModal);
+popupOverlay.addEventListener("click", hideModal);
 
 popupBtn.addEventListener("click", hideModal);
 btnClose.addEventListener("click", hideModal);
